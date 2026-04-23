8fb17e34a9aee1c1932a2bf23c59ea484f4bcc82
danrad1-lab <danrad1@live.kktavastia.fi>
2026-04-23 12:58:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Models import Seat, User
-from Database_functions import get_all_seats, seat_booking, set_user, delete_booking
+from Database_functions import get_all_seats, seat_booking, set_user, delete_booking, seat_check
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from jwt_and_email import send_email, create_token, verify_token
 app = FastAPI()
 
 
@@ -20,6 +21,8 @@ class SeatResponse(BaseModel):
     class Config:
         from_attributes = True
 
+class TokenRequest(BaseModel):
+    token: str
 
 class DeleteRequest(BaseModel):
     seat_id: int
@@ -50,7 +53,7 @@ def get_seats():
     return get_all_seats()
 
 @app.post("/booking")
-def book_seat(booking_requst: BookingRequest):
+def created_booking(booking_requst: BookingRequest):
 
     user = User(
         id=booking_requst.seat_id,
@@ -58,10 +61,31 @@ def book_seat(booking_requst: BookingRequest):
         last_name=booking_requst.last_name,
         email=booking_requst.email,
     )
+    if seat_check(user):
+        token = create_token(user.id, user.first_name, user.last_name, user.email)
+        send_email(user.email, token)
+        return {"message": "Check your email"}
 
-    set_user(user)
+    return {"message": "Something went wrong"}
 
-    return seat_booking(user)
+
+
+@app.post("/book_seat")
+def confirm_booking(tokenrequest: TokenRequest):
+    data = verify_token(tokenrequest.token)
+    user = User(
+        id=data.get("id"),
+        first_name=data.get("first_name"),
+        last_name=data.get("last_name"),
+        email=data.get("email")
+    )
+    if seat_check(user): # seat_check return true if place is free.
+        set_user(user)
+        return {"message": "Seat registered"}
+
+    return {"message": "Something went wrong"}
+
+
 
 @app.post("/delete_booking")
 def delete_book(delete_request: DeleteRequest):
