543abca97572d6e0380cedd157dedbf067ac0c8b
danrad1-lab <danrad1@live.kktavastia.fi>
2026-04-23 13:55:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ def confirm_booking(tokenrequest: TokenRequest):
     )
     if seat_check(user): # seat_check return true if place is free.
         set_user(user)
+        seat_booking(user)
         return {"message": "Seat registered"}
 
     return {"message": "Something went wrong"}
