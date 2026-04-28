b23421bd53bcc7558b8f35d5e2db28b04ab15189
danrad1-lab <danrad1@live.kktavastia.fi>
2026-04-28 12:01:24 +0300

    Update Datebase.py

--- Datebase.py
+++ Datebase.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres123:ipmbiwT6rTg9nzN13uZKccVz2mpTjj2W@dpg-d732ne24d50c73fg7jog-a/tavastiagames"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
